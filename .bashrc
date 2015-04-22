39d0f5b113715bb5d245d720453d4d589fab35ef
Matjaž Finžgar <matjaz.finzgar@gmail.com>
2015-04-22 18:48:29 +0200

    change panel and colors, add gnome-keyring

--- .bashrc
+++ .bashrc
@@ -6,8 +6,10 @@ alias ls='ls --color=auto'
 alias la='ls -la'
 alias vct='veracrypt -t'
 
+. ~/.git-prompt.sh
+. ~/.bash-prompt.sh
+
+eval $(gnome-keyring-daemon --start) && export SSH_AUTH_SOCK GPG_AGENT_INFO
 
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
-
-PS1="\[\e[36;1m\]\u \[\e[32;1m\]\W \[\e[31;1m\]> \[\e[0m\]"
--- .bin/dmenu_session.sh
+++ .bin/dmenu_session.sh
@@ -10,7 +10,8 @@ choice=$(echo -e "screenshot\nlock\nexit\nshutdown\nreboot" | $DMENU)
 
 case $choice in
 	screenshot) scrot ~/pics/screens/%Y-%m-%d-%T-screenshot.png & ;;
-	lock) xscreensaver-command -lock & ;;
+#	lock) xscreensaver-command -lock & ;;
+	lock) slimlock & ;;
 	exit) pkill -x panel; bspc quit & ;;
 	shutdown) sudo shutdown now & ;;
 	reboot) sudo reboot & ;;
