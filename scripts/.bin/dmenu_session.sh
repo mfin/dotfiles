514d2ae10cd8e9dbf035ecb6432773215d5b606b
Matjaž Finžgar <matjaz.finzgar@gmail.com>
2015-10-09 20:09:58 +0200

    add nmcli_dmenu to my dmenu menu :facepunch:

--- scripts/.bin/dmenu_session.sh
+++ scripts/.bin/dmenu_session.sh
@@ -6,7 +6,7 @@ else
 	DMENU='dmenu -i'
 fi
 
-choice=$(echo -e "lock\nexit\nshutdown\nreboot\nscreenshot" | $DMENU)
+choice=$(echo -e "lock\nnmcli\nscreenshot\nexit\nshutdown\nreboot" | $DMENU)
 
 case $choice in
 	screenshot) sleep 1 && scrot ~/pics/screens/%Y-%m-%d-%T-screenshot.png & ;;
@@ -14,4 +14,5 @@ case $choice in
 	exit) pkill -x panel; bspc quit & ;;
 	shutdown) systemctl poweroff & ;;
 	reboot) systemctl reboot & ;;
+	nmcli) nmcli_dmenu & ;;
 esac
