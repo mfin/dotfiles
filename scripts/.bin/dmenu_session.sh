3960a88a0cd1dca02a15e5d28d20f21a581f0ddf
Matjaž Finžgar <matjaz.finzgar@gmail.com>
2015-10-25 15:38:31 +0100

    new keybinds for screenshots

--- scripts/.bin/dmenu_session.sh
+++ scripts/.bin/dmenu_session.sh
@@ -6,10 +6,9 @@ else
 	DMENU='dmenu -i'
 fi
 
-choice=$(echo -e "lock\nnmcli\nscreenshot\nexit\nshutdown\nreboot" | $DMENU)
+choice=$(echo -e "lock\nnmcli\nexit\nshutdown\nreboot" | $DMENU)
 
 case $choice in
-	screenshot) sleep 1 && scrot ~/pics/screens/%Y-%m-%d-%T-screenshot.png & ;;
 	lock) slimlock & ;;
 	exit) pkill -x panel; bspc quit & ;;
 	shutdown) systemctl poweroff & ;;
