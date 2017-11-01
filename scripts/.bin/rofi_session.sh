8beb3d919dd402cc318965d177d57bb226be96b3
Matjaž Finžgar <matjaz@finzgar.net>
2017-11-01 19:27:16 +0100

    remove exit option from session menu

--- scripts/.bin/rofi_session.sh
+++ scripts/.bin/rofi_session.sh
@@ -6,11 +6,10 @@ else
 	DMENU='dmenu -i'
 fi
 
-choice=$(echo -e "lock\nexit\nsuspend\nshutdown\nreboot" | rofi -dmenu)
+choice=$(echo -e "lock\nsuspend\nshutdown\nreboot" | rofi -dmenu)
 
 case $choice in
 	lock) xscreensaver-command --lock & ;;
-	exit) bspc quit & ;;
 	suspend) systemctl suspend & ;;
 	shutdown) systemctl poweroff & ;;
 	reboot) systemctl reboot & ;;
