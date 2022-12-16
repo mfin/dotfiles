57baa291f0145b63942e4047cecc40711813cb45
Matjaž Finžgar <matjaz@finzgar.net>
2022-12-16 16:34:09 +0100

    add custom nnn plugins

--- nnn.nix
+++ nnn.nix
@@ -3,5 +3,14 @@
 {
   programs.nnn = {
     enable = true;
+
+    plugins = {
+      src = ./nnn/plugins;
+
+      mappings = {
+        "w" = "wsl-open";
+        "e" = "edit-in-code";
+      };
+    };
   };
 }
