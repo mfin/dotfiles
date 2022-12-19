8ba5a480e866b79d2709fc3d2e65c9cc96fd8641
Matjaž Finžgar <matjaz@finzgar.net>
2022-12-19 12:58:23 +0100

    update instructions

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ set -e
 
 curl -L https://nixos.org/nix/install | sh
 
-nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
 home-manager switch
 
-sudo echo "$HOME/.nix-profile/bin/zsh" >> /etc/shells
+echo "$HOME/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
 chsh -s $HOME/.nix-profile/bin/zsh
