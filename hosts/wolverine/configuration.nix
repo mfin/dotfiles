8cc0631c90e19fcddeba5d07d1310a69ab89fd94
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-28 11:21:28 +0200

    fix: consolidate

--- hosts/wolverine/configuration.nix
+++ hosts/wolverine/configuration.nix
@@ -114,11 +114,15 @@ in
     # user defined fonts
     # the reason there's Noto Color Emoji everywhere is to override DejaVu's
     # B&W emojis that would sometimes show instead of some Color emojis
-    fontconfig.defaultFonts = {
-      serif = ["Noto Serif" "Noto Color Emoji"];
-      sansSerif = ["Noto Sans" "Noto Color Emoji"];
-      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
-      emoji = ["Noto Color Emoji"];
+    fontconfig = {
+      hinting.autohint = true;
+
+      defaultFonts = {
+        serif = ["Noto Serif" "Noto Color Emoji"];
+        sansSerif = ["Noto Sans" "Noto Color Emoji"];
+        monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
+      };
     };
   };
 
