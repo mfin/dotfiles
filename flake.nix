32bbff2214b52262045327c21ce9cd0e9b8f8988
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-31 10:15:55 +0200

    fix: add hardware and update flakes

--- flake.nix
+++ flake.nix
@@ -7,8 +7,14 @@
     # sops-nix.url = "github:Mic92/sops-nix";
     # sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware = {
+      url = "github:nixos/nixos-hardware";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
@@ -21,7 +27,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, hyprland, ... }@inputs: {
+  outputs = { nixpkgs, nixos-hardware, home-manager, hyprland, ... }@inputs: {
     nixosConfigurations = {
       # laptop
       beast = nixpkgs.lib.nixosSystem {
@@ -29,6 +35,8 @@
         modules = [
           ./hosts/beast
           hyprland.nixosModules.default
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-gpu-nvidia-disable
         ];
       };
 
