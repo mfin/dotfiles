b556bf6d0330ab923089d0903ad7ba5bf970444a
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-31 10:17:45 +0200

    fix: workaround for latest unstable issues

--- hosts/beast/configuration.nix
+++ hosts/beast/configuration.nix
@@ -171,6 +171,7 @@ in
 
   hardware.opengl = {
     enable = true;
+    mesaPackage = pkgs.mesa;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
